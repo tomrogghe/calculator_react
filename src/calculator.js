816254a9123b7444f0e47a2e6ddf2ccf29284638
tom <tomrogghe@gmail.com>
2018-05-08 15:26:12 +0200

    done! with reset function

--- src/calculator.js
+++ src/calculator.js
@@ -4,19 +4,21 @@ class Calculator extends PureComponent {
   constructor() {
     super()
     this.state = {
-      input: ""
+      input: "",
+      done: "0"
     }
   }
 
   render() {
-    const { input } = this.state
+    const { input, done } = this.state
 
     return (
       <div className="viewport">
         <input type='text' value={input} />
         <div className="Buttons">
-          {['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/'].map(val => this.renderButton(input, val))}
-          <button onClick={() => this.setState({ input: eval(input) })}>
+          {['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map(val => this.renderNumbers(input, done, val))}
+          {['+', '-', '*', '/'].map(val => this.renderButton(input, val))}
+          <button onClick={() => this.setState({ input: eval(input), done: "1" })}>
             =
           </button>
           <button onClick={() => this.setState({ input: "" })}>
@@ -29,7 +31,20 @@ class Calculator extends PureComponent {
 
   renderButton(input, val) {
     return (
-      <button onClick={() => this.setState({ input: input + val })} >
+      <button onClick={() => this.setState({ input: input + val, done: "0" })} >
+        {val}
+      </button>)
+  }
+
+  renderNumbers(input, done, val) {
+    if (done === "0") {
+      return (
+        <button onClick={() => this.setState({ input: input + val })} >
+          {val}
+        </button>)
+    }
+    return (
+      <button onClick={() => this.setState({ input: val, done: "0" })} >
         {val}
       </button>)
   }
