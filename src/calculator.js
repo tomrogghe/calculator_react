a1b0b3f021abc2a82a3526a6c02d3c90b85fd473
tom <tomrogghe@gmail.com>
2018-05-08 15:14:14 +0200

    first full working version

--- src/calculator.js
+++ src/calculator.js
@@ -4,7 +4,7 @@ class Calculator extends PureComponent {
   constructor() {
     super()
     this.state = {
-      input: "zf"
+      input: ""
     }
   }
 
@@ -12,15 +12,26 @@ class Calculator extends PureComponent {
     const { input } = this.state
 
     return (
-      <div className="Device">
+      <div className="viewport">
         <input type='text' value={input} />
         <div className="Buttons">
+          {['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/'].map(val => this.renderButton(input, val))}
+          <button onClick={() => this.setState({ input: eval(input) })}>
+            =
+          </button>
           <button onClick={() => this.setState({ input: "" })}>
             clr
           </button>
         </div>
       </div>
     );
+  }
+
+  renderButton(input, val) {
+    return (
+      <button onClick={() => this.setState({ input: input + val })} >
+        {val}
+      </button>)
   }
 
 }
